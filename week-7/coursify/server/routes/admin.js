6310ccf8d35586fbc4a78eb1ec2f2ae92bc55543
Spandan-Mishra <spandanmishra69@gmail.com>
2024-10-29 00:16:46 +0530

    finished admin routes

--- week-7/coursify/server/routes/admin.js
+++ week-7/coursify/server/routes/admin.js
@@ -4,7 +4,9 @@ const jwt = require('jsonwebtoken');
 const ADMIN_SECRET = process.env.ADMIN_SECRET;
 const bcrypt = require('bcrypt');
 const { z } = require('zod');
+const adminMiddleware = require('../middleware/admin')
 const { Admin, Course } = require('../db');
+const admin = require('../middleware/admin');
 const adminSchema = z.object({
     username: z.string().min(3).max(20),
     password: z.string().min(8).max(16)
@@ -85,16 +87,65 @@ adminRouter.post('/admin/login', async (req, res) => {
     }  
 });
 
-adminRouter.post('/admin/courses', (req, res) => {
+adminRouter.post('/admin/courses', adminMiddleware, async (req, res) => {
     // logic to create a course
+    const { title, description, price, imageLink, published } = req.body;
+
+    const creatorId = req.id;
+
+    const course = await Course.create({
+        title: title,
+        description: description,
+        price: price,
+        imageLink: imageLink,
+        published: published,
+        creatorId: creatorId,
+    })
+
+    res.status(200).json({
+        message: "Course created successfully",
+        courseId: course._id.toString(),
+    })
+
 });
 
-adminRouter.put('/admin/courses/:courseId', (req, res) => {
+adminRouter.put('/admin/courses/:courseId', adminMiddleware,  async (req, res) => {
     // logic to edit a course
+    const courseId = req.params.courseId;
+    const adminId = req.id;
+    const { title, description, price, imageLink, published } = req.body;
+
+    try {
+        const course = await Course.updateOne({
+            _id: courseId,
+            creatorId: adminId,
+        }, {
+            title: title,
+            description: description,
+            price: price,
+            imageLink: imageLink,
+            published: published,
+        })
+
+        res.status(200).json({
+            message: "Course updated successfully",
+        })
+    } catch(e) {
+        res.status(400).json({
+            message:"Error while updating course"
+        })
+    }
+
 });
 
-adminRouter.get('/admin/courses', (req, res) => {
+adminRouter.get('/admin/courses', adminMiddleware, async (req, res) => {
     // logic to get all courses
+
+    const courses = await Course.find();
+
+    res.status(200).json({
+        courses: courses,
+    })
 });
 
 module.exports = { 
