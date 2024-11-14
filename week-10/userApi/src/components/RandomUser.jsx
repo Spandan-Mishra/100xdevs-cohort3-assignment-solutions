d33033023949d5c434c22b1e712f926a6d207c9e
Spandan-Mishra <spandanmishra69@gmail.com>
2024-11-14 19:40:32 +0530

    pagination added

--- week-10/userApi/src/components/RandomUser.jsx
+++ week-10/userApi/src/components/RandomUser.jsx
@@ -5,19 +5,29 @@ import axios from 'axios';
 const RandomUser = () => {
   const [page, setPage] = useState(0);
   const [users, setUsers] = useState([]);
+  const [currUsers, setCurrUsers] = useState([]);
 
   const fetchData = async () => {
     const response = await axios.get('https://randomuser.me/api/');
     const data = response.data.results[0];
-    // console.log(users);
-    setUsers([...users, data]);
+    
+    setUsers([...users, data]); 
+
+    console.log(users);
+    console.log(currUsers);
   }
   
+  const switchPage = (i) => {
+    setPage(i);
+    {10*(i+1) >= users.length ? setCurrUsers(users.slice(i * 10, users.length))
+     : setCurrUsers(users.slice(i * 10, i * 10 + 10))};
+  }
+
   const pageButtons = [];
-  for(let i = 0; i <= page; i++) {
+  for(let i = 0; i <= (users.length) / 10 ; i++) {
     pageButtons.push(<div className='cursor-pointer flex justify-center text-xl bg-black hover:bg-slate-800 text-white rounded-lg py-2 px-3' 
       key={i} 
-      onClick={() => setPage(i)}>
+      onClick={() => switchPage(i)}>
         {i + 1}
       </div>);
   }
@@ -26,18 +36,25 @@ const RandomUser = () => {
     fetchData();
   }, [])
 
+  useEffect(() => {
+    {10*(page+1) > users.length ? setCurrUsers(users.slice(page * 10, users.length))
+     : setCurrUsers(users.slice(page * 10, page * 10 + 10))};
+  }, [users]);
+
   return (
     <>
-      <div className='flex flex-wrap justify-center gap-8 px-5 mt-32 mb-20'>
-        {users.map((user) => (
+      {currUsers.length !== 0  ?
+        <div className='flex flex-wrap justify-center gap-8 px-5 mt-32 mb-20'>
+        {currUsers.map((user) => (
           <User user={user} key={user.login.uuid}/>
         ))}
       </div>
+      : <h1 className='text-8xl text-center mt-32 mb-20'>Loading...</h1>}
       <div className='flex flex-col items-center gap-8'>
         <div className='cursor-pointer flex justify-center text-xl bg-black hover:bg-slate-800 text-white rounded-lg py-2 w-48' onClick={fetchData}>
           Generate Users
         </div>
-        {page ? <div className='flex gap-2'>
+        {users.length > 10 ? <div className='flex gap-2'>
           {pageButtons}
         </div>
         : null}
@@ -47,7 +64,6 @@ const RandomUser = () => {
 }
 
 const User = ({ user }) => {
-  console.log(user);
   return (
     <div className='flex flex-col justify-center items-center py-5 border rounded-lg gap-2 w-72 h-52'>
       <img src={user.picture.medium} className='rounded-full w-20 h-20' alt="user" />
