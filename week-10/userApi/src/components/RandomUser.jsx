c853c66f3226d6521220089e367b37e73f11de8e
Spandan-Mishra <spandanmishra69@gmail.com>
2024-11-14 16:43:48 +0530

    fetch and display added

--- week-10/userApi/src/components/RandomUser.jsx
+++ week-10/userApi/src/components/RandomUser.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './RandomUser.css'
+import axios from 'axios';
 
 const RandomUser = () => {
+  const [page, setPage] = useState(0);
+  const [users, setUsers] = useState([]);
+
+  const fetchData = async () => {
+    const response = await axios.get('https://randomuser.me/api/');
+    const data = response.data.results[0];
+    // console.log(users);
+    setUsers((prevData) => [...prevData, data]);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
   return (
-    <div>RandomUser</div>
+    <>
+      <div>
+        {users.map((user) => (
+          <User user={user} key={user.login.uuid}/>
+        ))}
+      </div>
+      <div className='cursor-pointer' onClick={fetchData}>
+        Generate Users
+      </div>
+    </>
+  )
+}
+
+const User = ({ user }) => {
+  console.log(user);
+  return (
+    <div className='flex flex-col justify-center items-center py-3 border'>
+      <img src={user.picture.medium} className='rounded-full' alt="user" />
+      <h1>{user.name.first} {user.name.last}</h1>
+    </div>
   )
 }
 
