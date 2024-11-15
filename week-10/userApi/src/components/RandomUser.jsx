0acd3664fa85a96e196f67cca451367e494e4bad
Spandan-Mishra <spandanmishra69@gmail.com>
2024-11-15 23:17:47 +0530

    motions added

--- week-10/userApi/src/components/RandomUser.jsx
+++ week-10/userApi/src/components/RandomUser.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './RandomUser.css'
+import { motion } from 'motion/react';
 import axios from 'axios';
 
 const RandomUser = () => {
@@ -25,11 +26,19 @@ const RandomUser = () => {
 
   const pageButtons = [];
   for(let i = 0; i <= (users.length) / 10 ; i++) {
-    pageButtons.push(<div className='cursor-pointer flex justify-center text-xl bg-black hover:bg-slate-800 text-white rounded-lg py-2 px-3' 
+    pageButtons.push(<motion.div 
+      className='cursor-pointer flex justify-center text-xl bg-black hover:bg-slate-800 text-white rounded-lg py-2 px-3' 
+      whileHover={{
+        scale: 1.1,
+        boxShadow: '10px 8px 15px rgba(0, 0, 0, 0.2)',
+        backgroundColor: 'teal',
+        color: 'black',
+        transition: { duration: 0.2 }
+      }}
       key={i} 
       onClick={() => switchPage(i)}>
         {i + 1}
-      </div>);
+      </motion.div>);
   }
 
   useEffect(() => {
@@ -51,9 +60,19 @@ const RandomUser = () => {
       </div>
       : <h1 className='text-8xl text-center mt-32 mb-20'>Loading...</h1>}
       <div className='flex flex-col items-center gap-8'>
-        <div className='cursor-pointer flex justify-center text-xl bg-black hover:bg-slate-800 text-white rounded-lg py-2 w-48' onClick={fetchData}>
+        <motion.div 
+          className='cursor-pointer flex justify-center text-xl bg-black text-white rounded-lg py-2 w-48' 
+          onClick={fetchData}
+          whileHover={{
+            scale: 1.1,
+            boxShadow: '10px 8px 15px rgba(0, 0, 0, 0.2)',
+            backgroundColor: 'teal',
+            color: 'black',
+            transition: { duration: 0.2 }
+          }}
+          >
           Generate Users
-        </div>
+        </motion.div>
         {users.length > 10 ? <div className='flex gap-2'>
           {pageButtons}
         </div>
@@ -65,10 +84,18 @@ const RandomUser = () => {
 
 const User = ({ user }) => {
   return (
-    <div className='flex flex-col justify-center items-center py-5 border rounded-lg gap-2 w-72 h-52 bg-teal-600 border-black border-2'>
+    <motion.div 
+      className='flex flex-col justify-center items-center py-5 border rounded-lg gap-2 w-72 h-52 bg-teal-600 border-black border-2'
+      whileHover={{
+        y: -10,
+        scale: 1.05,
+        boxShadow: '10px 8px 15px rgba(0, 0, 0, 0.2)',
+        transition: { duration: 0.2 }
+      }}
+      >
       <img src={user.picture.medium} className='rounded-full w-20 h-20' alt="user" />
       <h1 className='text-2xl'>{user.name.first} {user.name.last}</h1>
-    </div>
+    </motion.div>
   )
 }
 
